623db7430c578f503caa4886b90e7b38c5c37a3f
gabrieljamc <160360773+gabrieljamc@users.noreply.github.com>
2025-04-08 21:33:51 +0200

    agregado a la clase fachada, ingrediente y plato

--- Proyecto Call-Eat/Call_Eat/src/Negocio/Fachada.java
+++ Proyecto Call-Eat/Call_Eat/src/Negocio/Fachada.java
@@ -1,5 +1,32 @@
 package Negocio;
 
-public interface Fachada {
+import java.util.List;
 
+public interface Fachada {
+	// -------------------
+    // MÉTODOS EMPLEADO
+    // -------------------
+    boolean registrarEmpleado(TransferEmpleado empleado);
+    boolean loginEmpleado(String correo, String contraseña);
+    TransferEmpleado obtenerEmpleadoSeguro(String correo, String contraseña);
+    boolean eliminarEmpleado(TransferEmpleado empleado);
+    String buscarIdEmpleado(String correo, String contraseña);
+
+    // -----------------------
+    // MÉTODOS INGREDIENTE
+    // -----------------------
+    boolean registrarIngrediente(TransferIngrediente ingrediente);
+    boolean modificarIngrediente(TransferIngrediente ingrediente);
+    boolean eliminarIngrediente(TransferIngrediente ingrediente);
+    TransferIngrediente buscarIngredientePorNombre(String nombre);
+    List<TransferIngrediente> listarIngredientes();
+
+    // -----------------------
+    // MÉTODOS PLATO
+    // -----------------------
+    boolean registrarPlato(TransferPlato plato);
+    boolean modificarPlato(TransferPlato plato);
+    boolean eliminarPlato(TransferPlato plato);
+    TransferPlato buscarPlatoPorNombre(String nombre);
+    List<TransferPlato> listarPlatos();
 }
