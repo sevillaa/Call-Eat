fab48e39794a26ca31053da31b8e8fe74f0a9068
gabrieljamc <160360773+gabrieljamc@users.noreply.github.com>
2025-04-08 21:33:01 +0200

    DAOPLATO

--- Proyecto Call-Eat/Call_Eat/src/Integracion/DAOPlato.java
+++ Proyecto Call-Eat/Call_Eat/src/Integracion/DAOPlato.java
@@ -5,7 +5,7 @@ import java.security.NoSuchAlgorithmException;
 import Negocio.TransferPlato;
 
 public interface DAOPlato {
-	public boolean registratPlato(TransferPlato idPlato) throws NoSuchAlgorithmException;
+	public boolean registrarPlato(TransferPlato idPlato) throws NoSuchAlgorithmException;
 	public TransferPlato buscarPlato(String idPlato);
 	public boolean actualizarPlato(String idPlato);
 	public boolean eliminarPlato(TransferPlato idPlato);
